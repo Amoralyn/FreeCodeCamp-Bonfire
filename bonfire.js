661a9b4f82208a533c38c529e2afff90243ed727
Amoralyn <lovelynisrael@hotmail.com>
2015-09-09 16:23:00 +0100

    Add more bonfire solutions

--- bonfire.js
+++ bonfire.js
@@ -395,3 +395,51 @@ function replace(str, before, after) {
 }
 
 replace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"); 
+
+// Pig String 
+
+function translate(str) {
+    // lowercase str for simplicity in testing
+    str = str.toLowerCase();
+    
+    // If a word begins with a vowel you just add "way" to the end.
+    var vowels = 'aeiou'.split('');
+    var startsWithVowel = false;
+    
+    vowels.forEach(function(entry) {
+        if (str.charAt(0) === entry) {
+            startsWithVowel = true;
+        }
+    });
+    
+    // If a word begins with a consonant, move it to the end and add "ay".
+    var consonants = "bcdfghjklmnpqrstvwxyz".split('');
+    var startsWithConsonant = false;
+    var tempChars = "";
+    
+    consonants.forEach(function(entry) {
+        if (str.charAt(0) === entry) {
+            startsWithConsonant = true;
+            tempChars += str.charAt(0);
+        }
+        
+        // is the next char also a consonant?
+        if (str.charAt(1) === entry) {
+            tempChars += str.charAt(1);
+        }
+    });
+    
+    
+    
+    if (startsWithVowel) {
+        return str + "way";
+    } else if (startsWithConsonant) {
+        if (tempChars.length > 1) {
+            return str.substring(2) + tempChars + "ay";
+        } else {
+            return str.substring(1) + tempChars + "ay";    
+        } 
+    }
+}
+
+translate("consonant");
